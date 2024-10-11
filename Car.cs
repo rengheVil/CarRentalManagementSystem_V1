2c195d35d4cb9474ec6ea249fcc3ed01b3b4c198
Rengheson V. <UT01205@unicomtic.com>
2024-10-11 14:22:32 +0530

    02commit

--- Car.cs
+++ Car.cs
@@ -3,5 +3,12 @@ using System;
 namespace CarRentalManagementSystem_V1{
 
 
-   
+    public class Car{
+        private string carId {get; set;}
+        private string brand {get; set;}
+        private string model {get; set;}
+        private decimal rentalPrice {get; set;}
+
+
+    }
 }
