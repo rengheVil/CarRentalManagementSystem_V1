7f0d1dbee2cd263434237b7f344256de252e6e43
Rengheson V. <UT01205@unicomtic.com>
2024-10-11 14:19:11 +0530

    01commit

---
+++ Car.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace CarRentalManagementSystem_V1{
+
+
+   
+}
--- obj/Debug/net8.0/CarRentalManagementSystem_V1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarRentalManagementSystem_V1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalManagementSystem_V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3549138564eb2fb4cbb217d61b984965a48ad3e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalManagementSystem_V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalManagementSystem_V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
